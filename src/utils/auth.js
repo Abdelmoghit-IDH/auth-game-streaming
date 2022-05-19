de766918d2b1a5765d00a1cd13ac93000059c060
Abdelmoghit <idhsaine.abdelmoghit@inemail.ine.inpt.ma>
2022-05-19 22:26:34 +0100

    get all user info from jwt

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,6 +1,7 @@
 const bcrypt = require('bcrypt');
 const jwt =  require('jsonwebtoken');
 const randomBytes = require('random-bytes');
+const { firstname } = require('../databases/adapters/sequelize/user-schema');
 
 const hashPassword = async (password) => {
   return await bcrypt.hash(password, 10);
@@ -10,9 +11,8 @@ const checkPassword = async (password, hashedPassword) => {
   return await bcrypt.compare(password, hashedPassword);
 };
 
-const generateAuthToken = (userId, email, isAdmin, tokenSecret, tokenExpiry, kid) => {
-  const authToken   = randomBytes(32).toString('hex');
-  const tokenData   = { userId, email, isAdmin, authToken };
+const generateAuthToken = (user, tokenSecret, tokenExpiry, kid) => {
+  const tokenData   = user;
   const signedToken = jwt.sign(tokenData, tokenSecret, { //TODO: change 
     algorithm: "HS256",
     expiresIn: tokenExpiry,
